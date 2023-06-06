aa3a73be794319257a3560df812bc26736950d10
Dustin-Davis <drpookydavis@comcast.net>
2023-06-06 13:38:15 -0700

    created a new table for users

--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -7,8 +7,21 @@ create schema "public";
 CREATE TABLE "game" (
     "gameId" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
     "gameName" TEXT NOT NULL,
+    "gameImage" TEXT NOT NULL,
     "createdAt" TIMESTAMPTZ NOT NULL default now(),
     CONSTRAINT "games_pk" PRIMARY KEY ("gameId")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "user" (
+    "userId" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    "userName" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
+    "googleAuth" TEXT NOT NULL,
+    "profileImage" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ NOT NULL default now(),
+    CONSTRAINT "user_pk" PRIMARY KEY ("userId")
 ) WITH (
   OIDS=FALSE
 );
