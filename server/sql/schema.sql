8143570711d0039a11234e58b7b1439869cc96ce
Dustin-Davis <drpookydavis@comcast.net>
2023-06-07 14:34:52 -0700

    created a table for raidInstance

--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -12,3 +12,19 @@ CREATE TABLE "game" (
 ) WITH (
   OIDS=FALSE
 );
+
+CREATE TABLE "raidInstance" (
+    "raidInstanceId" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    "raidId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "runTime" TIME NOT NULL,
+    "raidLeader" TEXT NOT NULL,
+    "selectedRoles" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ NOT NULL default now(),
+    CONSTRAINT "raidInstance_pk" PRIMARY KEY ("raidInstanceId")
+) WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE "raidInstance" ADD CONSTRAINT "raidInstance_fk0" FOREIGN KEY ("raidId") REFERENCES "raid"("raidId")
+ALTER TABLE "raidInstance" ADD CONSTRAINT "raidInstance_fk0" FOREIGN KEY ("gameId") REFERENCES "game"("gameId")
