e36febaa79a9f772e665c20833c247846dd49ff8
Dustin-Davis <drpookydavis@comcast.net>
2023-06-09 12:49:36 -0700

    added snackcase

--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -12,14 +12,14 @@ CREATE TABLE "game" (
 );
 
 CREATE TABLE "raidInstance" (
-    "raidInstanceId" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
-    "raidId" INTEGER NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "runTime" TIME NOT NULL,
-    "raidLeader" TEXT NOT NULL,
-    "selectedRoles" TEXT NOT NULL,
-    "createdAt" TIMESTAMPTZ NOT NULL default now(),
-    CONSTRAINT "raidInstance_pk" PRIMARY KEY ("raidInstanceId")
+    "raid_Instance_Id" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    "raid_Id" INTEGER NOT NULL,
+    "user_Id" INTEGER NOT NULL,
+    "run_Time" TIME NOT NULL,
+    "raid_Leader" TEXT NOT NULL,
+    "selected_Roles" TEXT NOT NULL,
+    "created_At" TIMESTAMPTZ NOT NULL default now(),
+    CONSTRAINT "raidInstance_pk" PRIMARY KEY ("raid_Instance_Id")
 );
 
-ALTER TABLE "raidInstance" ADD CONSTRAINT "raidInstance_fk0" FOREIGN KEY ("raidId") REFERENCES "raid"("raidId")
+ALTER TABLE "raidInstance" ADD CONSTRAINT "raidInstance_fk0" FOREIGN KEY ("raid_Id") REFERENCES "raid"("raid_Id")
