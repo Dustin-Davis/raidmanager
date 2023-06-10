420c33c4778c87cdb57a4ef9d589cf57dc390209
WhiskeyJack96 <Jacob.mikesell96@gmail.com>
2023-06-10 12:36:42 -0700

    Update server/sql/schema.sql

--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE "raid_instance" (
     "raid_leader" TEXT NOT NULL,
     "selected_roles" TEXT NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL default now(),
-    CONSTRAINT "raid_instance_pk" PRIMARY KEY ("raid_instance_Id")
+    CONSTRAINT "raid_instance_pk" PRIMARY KEY ("raid_instance_id")
 );
 
-ALTER TABLE "raid_instance" ADD CONSTRAINT "raid_instance_fk0" FOREIGN KEY ("raid_Id") REFERENCES "raid"("raid_Id")
+ALTER TABLE "raid_instance" ADD CONSTRAINT "raid_instance_fk0" FOREIGN KEY ("raid_id") REFERENCES "raid"("raid_id")
