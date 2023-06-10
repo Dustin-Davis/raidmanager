e230cdb22e43fc38657d719e08cc2d85c7467ef5
WhiskeyJack96 <Jacob.mikesell96@gmail.com>
2023-06-10 12:37:51 -0700

    Update server/sql/schema.sql

--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -5,21 +5,19 @@ drop schema "public" cascade;
 create schema "public";
 
 CREATE TABLE "game" (
-    "gameId" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
-    "gameName" TEXT NOT NULL,
-    "gameImage" TEXT NOT NULL,
-    "createdAt" TIMESTAMPTZ NOT NULL default now(),
-    CONSTRAINT "games_pk" PRIMARY KEY ("gameId")
-) WITH (
-  OIDS=FALSE
+    "game_id" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    "game_name" TEXT NOT NULL,
+    "game_image" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL default now(),
+    CONSTRAINT "games_pk" PRIMARY KEY ("game_id")
 );
 
 CREATE TABLE "user" (
-    "user_Id" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
-    "user_Name" TEXT NOT NULL,
-    "display_Name" TEXT NOT NULL,
-    "google_Auth" TEXT NOT NULL,
-    "profile_Image" TEXT NOT NULL,
-    "created_At" TIMESTAMPTZ NOT NULL default now(),
-    CONSTRAINT "user_pk" PRIMARY KEY ("userId")
+    "user_id" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    "user_name" TEXT NOT NULL,
+    "display_name" TEXT NOT NULL,
+    "google_auth" TEXT NOT NULL,
+    "profile_image" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL default now(),
+    CONSTRAINT "user_pk" PRIMARY KEY ("user_id")
 );
