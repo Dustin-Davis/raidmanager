9a532b97e3ee59543cabf17a244d9c99a9c3c860
WhiskeyJack96 <Jacob.mikesell96@gmail.com>
2023-06-10 12:36:12 -0700

    Apply suggestions from code review

--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -11,15 +11,15 @@ CREATE TABLE "game" (
     CONSTRAINT "games_pk" PRIMARY KEY ("gameId")
 );
 
-CREATE TABLE "raidInstance" (
-    "raid_Instance_Id" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
-    "raid_Id" INTEGER NOT NULL,
-    "user_Id" INTEGER NOT NULL,
-    "run_Time" TIME NOT NULL,
-    "raid_Leader" TEXT NOT NULL,
-    "selected_Roles" TEXT NOT NULL,
-    "created_At" TIMESTAMPTZ NOT NULL default now(),
-    CONSTRAINT "raidInstance_pk" PRIMARY KEY ("raid_Instance_Id")
+CREATE TABLE "raid_instance" (
+    "raid_instance_id" INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    "raid_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "run_time" TIME NOT NULL,
+    "raid_leader" TEXT NOT NULL,
+    "selected_roles" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL default now(),
+    CONSTRAINT "raid_instance_pk" PRIMARY KEY ("raid_instance_Id")
 );
 
-ALTER TABLE "raidInstance" ADD CONSTRAINT "raidInstance_fk0" FOREIGN KEY ("raid_Id") REFERENCES "raid"("raid_Id")
+ALTER TABLE "raid_instance" ADD CONSTRAINT "raid_instance_fk0" FOREIGN KEY ("raid_Id") REFERENCES "raid"("raid_Id")
