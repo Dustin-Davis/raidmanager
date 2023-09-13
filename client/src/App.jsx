3e68f20400078f3ef8a8b7a2f48c9bb5e26d66c2
Dustin-Davis <drpookydavis@comcast.net>
2023-09-13 13:33:15 -0700

    user had the ability to sign in/out

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,20 @@ import jwt_decode from "jwt-decode"
 import './App.css'
 
 function App() {
+  const [ user, setUser ] = useState({})
 
   function handelCallbackResponse(response) {
     console.log('Encoded JWT ID Token: ' + response.credential);
     var userObject = jwt_decode(response.credential)
     console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
   }
 
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
 
   useEffect(() => {
     // this is here so the linter doesn't see google as  not defined
@@ -27,6 +34,16 @@ function App() {
   return (
     <>
   <div id='signInDiv'></div>
+  { Object.keys(user).length != 0 &&
+      <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
+  }
+
+  { user &&
+    <div>
+      <img src= {user.picture}></img>
+      <h3>{user.name}</h3>
+    </div>
+  }
     </>
   )
 }
